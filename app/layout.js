bb385869a9f6ca54f2be69194db33056b73b7e27
nikmat1996 <nikhilmathew1996@gmail.com>
2024-04-18 15:56:08 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster"
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700']
+  weight: ['300', '400', '500', '600', '700']
 })
 
 export const metadata = {
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,55 +19,55 @@ const footerText = {
 }
 
 const links = [
-  { id: 'mission', text: 'Our Mission' },
-  { id: 'importance', text: 'Importance of Sports' },
-  { id: 'participation', text: 'Events Participation' },
-  { id: 'performance', text: 'Performance Stats' },
-  { id: 'support', text: 'Support System' }
+  { id: 'mission', text: 'Our', text2: "Mission" },
+  { id: 'importance', text: 'Importance', text2: "of Sports" },
+  { id: 'participation', text: 'Events', text2: "Participation" },
+  { id: 'performance', text: 'Performance', text2: "Stats" },
+  { id: 'support', text: 'Support', text2: "System"  }
 ]
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
-          <div>
+    <footer className='px-5 py-20 pt-10 md:pt-0'>
+      <div className='w-full max-w-[1139px] mx-auto'>
+        <div className='flex flex-col  md:flex-row '>
+          <div className='basis-[210px]'>
             <Image src={footerLogo} alt='logo' className='bg-transparent' />
-            <p>{footerText.p1}</p>
-            <address>
-              <h4>Contact us</h4>
-              <a href={`mailto:${footerText.email}`} className='flex'>
-                <Mail />
+            <p className='text-[13px] leading-normal pb-4 md:pb-11 pt-2.5 md:pt-5 max-w-[210px]'>{footerText.p1}</p>
+            <address className='not-italic'>
+              <h4 className='text-[13px] leading-normal font-bold'>Contact us</h4>
+              <a href={`mailto:${footerText.email}`} className='flex text-[13px] leading-loose items-center gap-2'>
+                <Mail size={12}/>
                 {footerText.email}
               </a>
-              <a href={`tel:${footerText.phoneDummy}`} className='flex'>
-                <Phone />
+              <a href={`tel:${footerText.phoneDummy}`} className='flex text-[13px] leading-loose items-center gap-2'>
+                <Phone size={12}/>
                 {footerText.phoneDummy}
               </a>
             </address>
           </div>
-          <nav>
-            <ul>
+          <nav className='flex-1  pt-7 xl:px-16 md:pt-6 flex flex-col items-center justify-center'>
+            <ul className='text-[13px] w-full flex leading-[17px] md:text-[21px] md:leading-[27px] font-light flex-wrap gap-y-12 '>
               {links.map(link => (
-                <li key={link.id}>
-                  <a href={`#${link.id}`}>{link.text}</a>
+                <li key={link.id} className='w-1/3 shrink-0  '>
+                  <a href={`#${link.id}`} className='max-w-min'><p>{link.text}</p><p>{link.text2}</p></a>
                 </li>
               ))}
             </ul>
           </nav>
-          <div>
+          <div className='flex gap-7 justify-center pt-14 md:pt-20'>
             <Button size='icon' className='rounded-full bg-white'>
-              <Facebook color='black' size={20} />
+              <Facebook color='black' size={15} />
             </Button>
             <Button size='icon' className='rounded-full bg-white'>
-              <Instagram color='black' size={20} />
+              <Instagram color='black' size={15} />
             </Button>
-            <Button size='icon' className='rounded-full bg-white'>
-              <Twitter color='black' size={20} />
+            <Button size='icon' className='rounded-full bg-white '>
+              <Twitter color='black' size={15} />
             </Button>
           </div>
         </div>
-        <p>All rights reserved@buidachamp.com</p>
+        <p className='text-[10px] leading-3 md:text-sm md:leading-[18px] text-center py-4'>All rights reserved@buidachamp.com</p>
       </div>
     </footer>
   )
