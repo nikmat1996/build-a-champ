5b97d66501f63d50f0d976aa1a3abb3e843254f1
nikmat1996 <nikhilmathew1996@gmail.com>
2024-05-07 01:03:08 +0530

    form fe

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,3 +1,7 @@
+"use client"
+
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
 import React from 'react'
 import { Input } from '@/components/ui/input'
 import {
@@ -7,6 +11,19 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import TimePicker from 'react-time-picker';
+
 
 const sports = [
   { value: 'football', label: 'Football' },
@@ -21,19 +38,75 @@ const sports = [
   { value: 'hiking', label: 'Hiking' }
 ]
 
+const genderOpt = [
+  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' },
+  { value: 'both', label: 'Male and Female' }
+]
+
 const page = () => {
+
+  const [date, setDate] = React.useState()
+  const [value, onChange] = React.useState('10:00');
+
+
+
   return (
-    <section className='mx-auto h-screen max-w-4xl border-x-2  py-10'>
+    <section className='mx-auto max-w-4xl border-x-2  py-10'>
       <form action='' className='flex flex-col items-center gap-y-10'>
         <h2>ADD EVENT</h2>
+
+
         <Select name='sport'>
-          <SelectTrigger className='w-[200px]'>
+          <SelectTrigger className='w-full max-w-[400px]'>
             <SelectValue placeholder='Select Sport' />
           </SelectTrigger>
           <SelectContent>
             {sports.map(sport =><SelectItem key={sport.value} value={sport.value}>{sport.label}</SelectItem>)}
           </SelectContent>
         </Select>
+
+        <Popover >
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-full max-w-[400px] justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+
+        <TimePicker onChange={onChange} value={value} className={"w-full max-w-[400px] rounded-lg"}/>
+
+        <Select name='gender'>
+          <SelectTrigger className='w-full max-w-[400px]'>
+            <SelectValue placeholder='Select Gender' />
+          </SelectTrigger>
+          <SelectContent>
+            {genderOpt.map(opt =><SelectItem key={opt.value} value={opt.value}>{opt.label}</SelectItem>)}
+          </SelectContent>
+        </Select>
+
+        <Input type="text" name={"category"} placeholder="Category" className="w-full max-w-[400px]"/>
+        <Input type="number" name={"price"} placeholder="Price" className="w-full max-w-[400px]"/>
+        <textarea type="textarea" name={"address"} placeholder="Address" className="w-full max-w-[400px] border rounded-lg p-2 focus-within:outline-black "/>
+        <Input type="tel" name={"phone"} placeholder="Phone" className="w-full max-w-[400px]"/>
+
+        <Button type="submit" className="px-20">Add</Button>
+
       </form>
     </section>
   )
