e3d54bf4d96ae4eea0ce3f6e1c68c1e7f14cac36
nikmat1996 <nikhilmathew1996@gmail.com>
2024-05-13 00:31:20 +0530

    favicon working

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+      </head>
       <body className={poppins.className}>
         
         {children}
