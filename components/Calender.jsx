f05bb9e6733f954b529ec8e64dc114e7f6ee7409
nikmat1996 <nikhilmathew1996@gmail.com>
2024-05-12 11:04:46 +0530

    revalidate 30 sec

--- components/Calender.jsx
+++ components/Calender.jsx
@@ -76,7 +76,7 @@ const Calender = props => {
 
   const getData = async () => {
     try {
-      const response = await fetch('/api/events', { next: { tags: ['events'] }});
+      const response = await fetch('/api/events', { next: { revalidate: 30 } });
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
