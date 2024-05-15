7e6b6655accf60531fb210a274c91bbc231bdac6
nikmat1996 <nikhilmathew1996@gmail.com>
2024-05-15 14:30:59 +0530

    icon fix

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,9 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <head>
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
       </head>
       <body className={poppins.className}>
         
