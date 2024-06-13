c2f12e57b2f9bb6f1194bf43e42414aef73bb975
nikmat1996 <nikhilmathew1996@gmail.com>
2024-06-13 14:20:01 +0530

    add video

--- components/Performance.jsx
+++ components/Performance.jsx
@@ -1,26 +1,42 @@
-import Image from "next/image"
-import performance_lg from '../assets/images/performance_lg.jpg'
-// import performance_lg from '../assets/images/performance_lg.png'
-import performance_sm from '../assets/images/performance_sm.jpg'
+import Image from 'next/image'
 import performance from '../assets/gifs/performance.gif'
+// /Users/nm/Documents/code/proj/build-a-champ/public/PerformanceStat.mp4
 
 const performanceText = {
-    h2: "Performance stats",
-    p: "Coming soon to Build A Champ: Track your progress and elevate your game with our real-time performance stats! Hone your skills and see how you stack up against the competition. It's not just about playing—it’s about improving every day and mastering your sport!"
+  h2: 'Performance stats',
+  p: "Coming soon to Build A Champ: Track your progress and elevate your game with our real-time performance stats! Hone your skills and see how you stack up against the competition. It's not just about playing—it’s about improving every day and mastering your sport!"
 }
 
 const Performance = () => {
   return (
-    <section className="pt-1.5 md:pt-20 px-5 pb-14 md:pb-28" id="performance">
-      <div className='flex flex-col items-center w-full max-w-[1139px] mx-auto md:flex-row-reverse md:justify-between md:items-start'>
-        <Image alt="girl checking stats" src={performance_lg} className='hidden md:block self-center w-full rounded-[30px]'/>
-        <Image alt="girl checking stats small" src={performance_sm} className='md:hidden'/>
+    <section className='px-5 pb-14 pt-1.5 md:pb-28 md:pt-20' id='performance'>
+      <div className='mx-auto flex w-full max-w-[1139px] flex-col items-center md:flex-row-reverse md:items-start md:justify-between'>
+        {/* <Image
+          alt='girl checking stats'
+          src={performance_lg}
+          className='hidden w-full self-center rounded-[30px] md:block'
+        /> */}
+        {/* <Image alt="girl checking stats small" src={performance_sm} className='md:hidden'/> */}
+        <video className='w-full max-w-xl rounded-3xl' autoPlay loop muted playsInline preload='auto'>
+          <source src="/PerformanceStat.mp4" type='video/mp4'  />
+          
+          Your browser does not support the video tag.
+        </video>
         <div className='w-full max-w-[629px] '>
-            <div className='flex flex-row-reverse justify-center items-center md:flex-col md:items-start'>
-                <Image alt="graph gif" src={performance} className='w-[43px] h-[43px] md:w-[93px] md:h-[93px]' unoptimized/>
-                <h2 className='text-2xl/9 font-bold pt-[12px] pb-2.5 md:text-[50px] md:leading-[57px] md:pb-[30px] translate-y-1'>{performanceText.h2}</h2>
-            </div>
-            <p className='text-[15px] leading-5 md:text-xl/10 md:max-w-[480px] mx-auto md:mx-0'>{performanceText.p}</p>
+          <div className='flex flex-row-reverse items-center justify-center md:flex-col md:items-start'>
+            <Image
+              alt='graph gif'
+              src={performance}
+              className='h-[43px] w-[43px] md:h-[93px] md:w-[93px]'
+              unoptimized
+            />
+            <h2 className='translate-y-1 pb-2.5 pt-[12px] text-2xl/9 font-bold md:pb-[30px] md:text-[50px] md:leading-[57px]'>
+              {performanceText.h2}
+            </h2>
+          </div>
+          <p className='mx-auto text-[15px] leading-5 md:mx-0 md:max-w-[480px] md:text-xl/10'>
+            {performanceText.p}
+          </p>
         </div>
       </div>
     </section>
